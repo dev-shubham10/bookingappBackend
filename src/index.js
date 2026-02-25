07bff4e594ad03d0834676218c7701d8a5975d05
Shubham Ashok Shingare <136559144+2works-Shubham@users.noreply.github.com>
2026-02-25 20:15:55 +0530

    Fix CORS origin slash issue

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://bookingapp-frontend-git-main-dev-shubham10s-projects.vercel.app/"
+    "https://bookingapp-frontend-git-main-dev-shubham10s-projects.vercel.app"
   ],
   credentials: true
 })); 
