62f46a41771893ce59ceef704b1eb1410654e544
Shubham Ashok Shingare <136559144+2works-Shubham@users.noreply.github.com>
2026-02-26 11:47:35 +0530

    Frontend code

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -17,13 +17,6 @@
 
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
