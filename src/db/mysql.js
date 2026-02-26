71276816e2b2a9280e59f5319a262f69db445291
Shubham Ashok Shingare <136559144+2works-Shubham@users.noreply.github.com>
2026-02-26 18:54:28 +0530

    Code is ready to update

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST || 'localhost',
   port: process.env.MYSQLPORT ? Number(process.env.MYSQLPORT) : 3306,
   user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
+  password: process.env.MYSQLPASSWORD || 'Shubham@123',
   database: process.env.MYSQLDATABASE || 'booking_app',
   waitForConnections: true,
   connectionLimit: 10,
