448be4e43c3c811e7ef2180736157d05fc91c57e
Shubham Ashok Shingare <136559144+2works-Shubham@users.noreply.github.com>
2026-02-26 11:22:45 +0530

    Fixed cors issue

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,16 +1,27 @@
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'booking_app',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
+
+
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'booking_app',
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
-
